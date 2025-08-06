c7d370b785266b9f9bed5392e31287cbb36f278e
Vincent Li <vincent.mc.li@gmail.com>
2025-08-05 22:56:30 -0700

    xdp-geoip: add xdpgeoip init script control program
    Signed-off-by: Vincent Li <vincent.mc.li@gmail.com>

---
+++ src/misc-progs/xdpgeoipctrl.c
@@ -0,0 +1,40 @@
+/* This file is part of the BPFire Firewall.
+ *
+ * This program is distributed under the terms of the GNU General Public
+ * Licence.  See the file COPYING for details.
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include "setuid.h"
+
+int main(int argc, char *argv[]) {
+
+	if (!(initsetuid()))
+		exit(1);
+
+	if (argc < 2) {
+		fprintf(stderr, "\nNo argument given.\n\nxdpgeoipctrl (start|stop|restart)\n\n");
+		exit(1);
+	}
+
+	if (strcmp(argv[1], "start") == 0) {
+		safe_system("/etc/rc.d/init.d/xdpgeoip start");
+	} else if (strcmp(argv[1], "stop") == 0) {
+		safe_system("/etc/rc.d/init.d/xdpgeoip stop");
+	} else if (strcmp(argv[1], "restart") == 0) {
+		safe_system("/etc/rc.d/init.d/xdpgeoip restart");
+	} else if (strcmp(argv[1], "status") == 0) {
+		safe_system("/etc/rc.d/init.d/xdpgeoip status");
+	} else {
+		fprintf(stderr, "\nBad argument given.\n\nxdpgeoipctrl (start|stop|restart|status)\n\n");
+		exit(1);
+	}
+
+	return 0;
+}
