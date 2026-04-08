2c940c20f45cc0fbb0afea11bb7d6f4efd26ce24
Vincent Li <vincent.mc.li@gmail.com>
2026-04-07 19:36:29 -0700

    make.sh: New toolchain version for glibc update
    Signed-off-by: Vincent Li <vincent.mc.li@gmail.com>

--- make.sh
+++ make.sh
@@ -32,7 +32,7 @@ GIT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"			# Git Branch
 GIT_TAG="$(git tag | tail -1)"					# Git Tag
 GIT_LASTCOMMIT="$(git rev-parse --verify HEAD)"			# Last commit
 
-TOOLCHAINVER=20260321
+TOOLCHAINVER=20260407
 
 KVER_SUFFIX="-${SNAME}"
 
