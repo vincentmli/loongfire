dd07471749ca95e7e4851a43d6f82dcd36cac016
Vincent Li <vincent.mc.li@gmail.com>
2026-06-26 18:48:31 -0700

    python3-async-timeout: New package
    commit 33dd29a5557fbbfd434766bf6ee510f38612c4de
    Author: Michael Tremer <michael.tremer@ipfire.org>
    Date:   Wed May 20 09:32:40 2026 +0000
    
        python3-async-timeout: New package
    
        Required by python3-aiohttp.
    
        Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>
    
    Signed-off-by: Vincent Li <vincent.mc.li@gmail.com>

--- make.sh
+++ make.sh
@@ -2193,6 +2193,7 @@ fi
 	lfsmake2 python3-expandvars
 	lfsmake2 python3-propcache
 	lfsmake2 python3-yarl
+	lfsmake2 python3-async-timeout
 	lfsmake2 python3-aiohttp
 	lfsmake2 knot
 	lfsmake2 knot-resolver
