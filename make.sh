4995405e2a3eae0477a53ad9436999c8d33b615a
Vincent Li <vincent.mc.li@gmail.com>
2026-06-26 19:11:22 -0700

    python3-aiosignal: New package
    commit a33d4fb367c78cdae4b88580105a2a7a8914ec95
    Author: Michael Tremer <michael.tremer@ipfire.org>
    Date:   Wed May 20 10:04:11 2026 +0000
    
        python3-aiosignal: New package
    
        Required by python3-aiohttp.
    
        Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>
    
    Signed-off-by: Vincent Li <vincent.mc.li@gmail.com>

--- make.sh
+++ make.sh
@@ -2196,6 +2196,7 @@ fi
 	lfsmake2 python3-yarl
 	lfsmake2 python3-async-timeout
 	lfsmake2 python3-aiohappyeyeballs
+	lfsmake2 python3-aiosignal
 	lfsmake2 python3-aiohttp
 	lfsmake2 knot
 	lfsmake2 knot-resolver
